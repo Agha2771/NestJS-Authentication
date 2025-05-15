4dab628fe6581767b54d058a8e8c1fd1106465ec
umar-adly <muhammadumar@adly.com>
2025-05-15 12:04:31 +0500

    Cors issue

---
+++ src/main.ts
@@ -0,0 +1,48 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import { HttpException, HttpStatus, ValidationError, ValidationPipe } from '@nestjs/common';
+import express from 'express';
+import { join } from 'path';
+
+const server = express();
+
+async function createNestServer(expressInstance) {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+
+  app.enableCors({
+    origin: ['https://next-js-crud-qr8y.vercel.app', 'http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.flatMap((err) =>
+          Object.values(err.constraints || {}),
+        );
+        return new HttpException(
+          {
+            success: false,
+            message: 'Validation failed',
+            errors: messages,
+          },
+          HttpStatus.UNPROCESSABLE_ENTITY,
+        );
+      },
+    }),
+  );
+
+  await app.init();
+  return app;
+}
+
+const app = createNestServer(server);
+export default server; 
